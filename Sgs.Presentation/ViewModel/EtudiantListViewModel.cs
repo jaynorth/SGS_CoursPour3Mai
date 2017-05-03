253afefa13defcd5c2da005ea06327f33890b39a
jay <jayrdee@gmail.com>
2017-05-03 11:34:51 +0200

    ajout de EtudiantListViewModel dans viewModel

---
+++ Sgs.Presentation/ViewModel/EtudiantListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sgs.Presentation.ViewModel
+{
+    public class EtudiantListViewModel
+    {
+        private ObservableCollection<Etudiant> _etudiants;
+
+        public EtudiantListViewModel()
+        {
+            _etudiants = new ObservableCollection<Etudiant>();
+            _etudiants.Add(new Etudiant("Holmes", "Sherlock", "Baker Street", "121b", "1000", "London"));
+            _etudiants.Add(new Etudiant("Newton", "Isaac", "Gravity Street", "12345", "1045", "Bagdad"));
+
+
+     
+        }
+
+        public ObservableCollection<Etudiant> Etudiants
+        {
+            get { return _etudiants; }
+            set
+            {
+                _etudiants.Clear();
+                _etudiants = value;
+
+            }
+
+    }
+}
