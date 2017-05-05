0044b7f91cf88c045aa05ce0b8be0ab459dca872
jay <jayrdee@gmail.com>
2017-05-05 16:32:59 +0200

    Entity Models working, Changed EntityListViewModel to connect and use entity, partial classe created under EntitModel folders, references and app config linked from Presententation to Persistance file.

---
+++ Sgs.Persistance/EntityModel/partial_tbEtudiants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sgs.Persistance.EntityModel
+{
+    public partial class tbEtudiant
+    {
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Prenom, Nom);
+        }
+    }
+}
+
--- Sgs.Presentation/ViewModel/EtudiantListViewModel.cs
+++ Sgs.Presentation/ViewModel/EtudiantListViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Sgs.Business.Model;
+using Sgs.Persistance.EntityModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Sgs.Presentation.ViewModel
 {
@@ -14,15 +16,19 @@ namespace Sgs.Presentation.ViewModel
 
         public EtudiantListViewModel()
         {
-            _etudiants = new ObservableCollection<Etudiant>();
-            _etudiants.Add(new Etudiant("Holmes", "Sherlock", "Baker Street", "121b", "1000", "London"));
-            _etudiants.Add(new Etudiant("Newton", "Isaac", "Gravity Street", "12345", "1045", "Bagdad"));
+            EntityModelConnexion context = new EntityModelConnexion();
+            _etudiants = new ObservableCollection<tbEtudiant>(context.tbEtudiants);
+           
+            
+            // _etudiants = new ObservableCollection<Etudiant>();
+           // _etudiants.Add(new Etudiant("Holmes", "Sherlock", "Baker Street", "121b", "1000", "London"));
+           // _etudiants.Add(new Etudiant("Newton", "Isaac", "Gravity Street", "12345", "1045", "Bagdad"));
 
 
      
         }
-        private ObservableCollection<Etudiant> _etudiants;
-        public ObservableCollection<Etudiant> Etudiants
+        private ObservableCollection<tbEtudiant> _etudiants;
+        public ObservableCollection<tbEtudiant> Etudiants
         {
             get { return _etudiants; }
             set
